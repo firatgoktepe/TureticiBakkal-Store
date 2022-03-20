6865f8c9f36423a255b8c0f97f9fc0682e925f4f
Firat Göktepe <firat@naxnordic.com>
2022-03-20 18:01:57 +0300

    added date and fixed box layouts

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = ({ children }) => {
   return (
     <footer className={styles.footer}>
       <Container className={styles.footerContainer}>
-        &copy; Turetici Bakkal Store
+        &copy; Turetici Bakkal Store {new Date().getFullYear()}
       </Container>
     </footer>
   )
